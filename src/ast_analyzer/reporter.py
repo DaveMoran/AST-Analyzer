12e75e1151186d9facf2500a766812b786fa16b5
Dave Moran <dave.moran118@gmail.com>
2025-12-29 21:52:52 -0500

    add tests for reporter

--- src/ast_analyzer/reporter.py
+++ src/ast_analyzer/reporter.py
@@ -4,3 +4,7 @@ ast_analyzer.reporter
 Take the analysis of the project and generate a user friendly report to be displayed along with suggestions
 on how to improve said project
 """
+
+
+def reporter():
+    return "reporter"
---
+++ tests/test_reporter.py
@@ -0,0 +1,9 @@
+"""
+tests.test_reporter
+"""
+
+from ast_analyzer import reporter
+
+
+def test_reporter():
+    assert reporter() == "reporter"
