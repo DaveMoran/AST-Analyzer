46c0d1fa4faa2feb8c4ec16b3a38c85ee58e3eb8
Dave Moran <dave.moran118@gmail.com>
2025-12-29 21:06:08 -0500

    add placeholder for parser test

--- src/ast_analyzer/parser.py
+++ src/ast_analyzer/parser.py
@@ -3,3 +3,7 @@ ast_analyzer.parser
 
 Traverse the project and transform the data into a readable format for the analzyer to use
 """
+
+
+def parser():
+    return "parser"
---
+++ tests/test_parser.py
@@ -0,0 +1,9 @@
+"""
+tests.test_parser
+"""
+
+from ast_analyzer import parser
+
+
+def test_parser():
+    assert parser() == "parser"
