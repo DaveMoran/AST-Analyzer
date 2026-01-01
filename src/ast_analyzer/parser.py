e5c200fcb35e64cf6962935a1d543d0483c64247
Dave Moran <dave.moran118@gmail.com>
2026-01-01 18:34:16 -0500

    ASTANA-004: add class for AnalysisResult

--- src/ast_analyzer/parser.py
+++ src/ast_analyzer/parser.py
@@ -7,3 +7,25 @@ Traverse the project and transform the data into a readable format for the analz
 
 def parser():
     return "parser"
+
+
+class AnalysisResult:
+    """
+    Takes all of the findings from the CodeAnalyzer class and generates a report
+    to share with our user
+    """
+
+    def __init__(self):
+        self.results = []
+
+    def __len__(self):
+        """Return the number of items in the results list"""
+
+    def __bool__(self):
+        """Return whether or not theres at least 1 result in the result list"""
+
+    def __getitem__(self, index):
+        """Get a specific item from our results list"""
+
+    def __add__(self, other):
+        """Combine with results from other scans"""
