226f803016994fca7ec7912f3295533976cfd900
Dave Moran <dave.moran118@gmail.com>
2026-01-01 18:48:43 -0500

    ASTANA-4: updates to AnalysisResult for clarity and added function

--- src/ast_analyzer/parser.py
+++ src/ast_analyzer/parser.py
@@ -18,14 +18,27 @@ class AnalysisResult:
     def __init__(self):
         self.results = []
 
+    def __repr__(self):
+        return f"AnalysisResult(results={self.results})"
+
+    def __str__(self):
+        return f"Analysis Complete! There are {len(self)} changes to implement"
+
     def __len__(self):
         """Return the number of items in the results list"""
+        raise NotImplementedError("ASTANA-6 will implement")
 
     def __bool__(self):
         """Return whether or not theres at least 1 result in the result list"""
+        raise NotImplementedError("ASTANA-6 will implement")
 
     def __getitem__(self, index):
         """Get a specific item from our results list"""
+        raise NotImplementedError("ASTANA-6 will implement")
 
     def __add__(self, other):
         """Combine with results from other scans"""
+        raise NotImplementedError("ASTANA-6 will implement")
+
+    def __getitem__(self, i):
+        return self.results[i]
