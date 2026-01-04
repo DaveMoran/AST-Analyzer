9970023291a406b51d3d9343f2fb2fe016867adf
Dave Moran <dave.moran118@gmail.com>
2026-01-04 16:50:12 -0500

    ASTANA-5: add test cases for special methods

---
+++ tests/test_astnode.py
@@ -0,0 +1,33 @@
+import unittest
+from ast_analyzer.ASTNode import ASTNode
+
+
+class TestASTNode(unittest.TestCase):
+
+    def setUp(self):
+        """TODO - hook method for setting up fixtures before each test method is called"""
+        return
+
+    def test_len(self):
+        """TODO - Ensure that the pythonic len function works"""
+        return
+
+    def test_getitem(self):
+        """TODO - Ensure that index retrieval works"""
+        return
+
+    def test_repr(self):
+        """TODO - Ensure that repr returns debugging information"""
+        return
+
+    def test_str(self):
+        """TODO - Ensure that str returns a user friendly string"""
+        return
+
+    def test_hash(self):
+        """TODO - Ensure that ASTNodes can be added to sets/dics"""
+        return
+
+    def test_iter(self):
+        """TODO - Ensure for loops can iterate over nodes"""
+        return
