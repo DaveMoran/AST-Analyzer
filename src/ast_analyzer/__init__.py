9e71185432ee0db36969a3f1a5fa0f584717874a
Dave Moran <dave.moran118@gmail.com>
2026-01-04 18:14:52 -0500

    ASTANA-5: add docstring to init

--- src/ast_analyzer/__init__.py
+++ src/ast_analyzer/__init__.py
@@ -1,10 +1,4 @@
-"""
-AST Analyzer: Traverst the syntax tree of a user's project and provide a score or suggestion on how to improve the
-quality of the code
-"""
-
-import ast
-from typing import Optional
+"""Initialize AST Analyzer project"""
 
 
 def main():
