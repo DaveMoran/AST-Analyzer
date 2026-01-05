314d08d32aa0f844f49860591ce135172a82924b
Dave Moran <dave.moran118@gmail.com>
2026-01-04 19:18:56 -0500

    ASTANA-5: add return types for special methods

--- src/ast_analyzer/ASTNode.py
+++ src/ast_analyzer/ASTNode.py
@@ -28,27 +28,27 @@ class ASTNode:
         for child in ast.iter_child_nodes(self.node):
             self.children.append(ASTNode(child, self))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"ASTNode({type(self.node).__name__})"
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.children)
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int) -> "ASTNode":
         return self.children[i]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"AST Node | Children: {len(self)}"
 
     def __iter__(self):
         for child in self.children:
             yield child
 
-    def __contains__(self, item):
+    def __contains__(self, item) -> bool:
         return item in self.children
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.node == other.node
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.node, self.parent))
