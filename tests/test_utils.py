421afcba9d07e6700d433877a0b2cd5ad8ad5a75
Dave Moran <dave.moran118@gmail.com>
2026-01-16 05:45:00 -0500

    ASTANA-9: import testing from exercises

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+from ast_analyzer.utils import ast_timing
+
+
+@ast_timing()
+def factorial(n):
+    return 1 if n < 2 else n * factorial(n - 1)
+
+
+def test_prints_timing_output(capsys):
+    """Decorator should print timing information to stdout."""
+
+    factorial(3)
+    captured = capsys.readouterr()
+    assert "factorial" in captured.out
+    assert "->" in captured.out
