a28de046d5caa80fa87c5764b6be9c9f0227ca65
Dave Moran <dave.moran118@gmail.com>
2026-01-19 16:14:12 -0500

    ASTANA-7: Start bringing it together in main

--- src/ast_analyzer/main.py
+++ src/ast_analyzer/main.py
@@ -6,7 +6,14 @@ Entry point for the AST Analyzer application
 
 
 def main():
-    print("Hello world!")
+    # Step 1: Ask user for directory of files
+    # Step 2: Filter out all invalid files from directory
+    # Step 3: Start iterating through each file
+    # Step 4: Parse through the lines of each file
+    # Step 5: Create an AST Node of each file
+    # Step 6: Run the nodes through our analysis
+    # Step 7: Generate a report based on findings
+    print("Hello world")
 
 
 if __name__ == "__main__":
