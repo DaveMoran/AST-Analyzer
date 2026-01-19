cd5ad320ed3864ba1127bf1ecf402f35ef2f495c
Dave Moran <dave.moran118@gmail.com>
2026-01-19 16:19:56 -0500

    ASTANA-7: add steps 1 - 3

--- src/ast_analyzer/main.py
+++ src/ast_analyzer/main.py
@@ -4,11 +4,19 @@ ast_analyzer.main
 Entry point for the AST Analyzer application
 """
 
+from ast_analyzer.generators.file_traversal import get_working_files
+
 
 def main():
     # Step 1: Ask user for directory of files
+    directory = "./src/ast_analyzer"  # TODO - swap out with CLI command when we get to that story
+
     # Step 2: Filter out all invalid files from directory
+    working_files = get_working_files(directory)
+
     # Step 3: Start iterating through each file
+    for file in working_files:
+        print(file)
     # Step 4: Parse through the lines of each file
     # Step 5: Create an AST Node of each file
     # Step 6: Run the nodes through our analysis
