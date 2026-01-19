98746794882b2b62666a1bf0af93da3391e858e3
Dave Moran <dave.moran118@gmail.com>
2026-01-19 14:47:53 -0500

    chore: move timing decorator to separate file

--- src/ast_analyzer/utils.py
+++ src/ast_analyzer/utils.py
@@ -6,51 +6,8 @@ A list of helper functions that can be reused throughout the application
 
 import functools
 import logging
-import time
 
 from typing import Callable, Any, Optional
-
-DEFAULT_FMT = "[{curr_time} | {time_taken:0.2f}s] {fn_name}({args}) -> {result}"
-
-
-class ast_timing:
-    """
-    Custom decorator that allows for functions to have timing metrics display per run
-    """
-
-    def __init__(self, fn: Callable, fmt: str = DEFAULT_FMT):
-        functools.wraps(fn)(self)
-        self.fn = fn
-        self.fmt = fmt
-        self.accumulated_time = 0
-        self.times_called = 0
-
-    def __call__(self, *args: Any, **kwargs: Any):
-        # Start timer
-        curr_time = time.strftime("%H:%M:%S", time.localtime())
-        start_time = time.perf_counter()
-
-        # Run function
-        _result = self.fn(*args, **kwargs)
-
-        # Get time taken, add to free variables
-        time_taken = time.perf_counter() - start_time
-        self.accumulated_time += time_taken
-        self.times_called += 1
-
-        # Populate logger variables
-        fn_name = self.fn.__name__
-        args = ", ".join(repr(arg) for arg in args)
-        result = repr(_result)
-
-        # Print timing
-        print(self.fmt.format(**locals()))
-        if time_taken > 0.5:
-            print(f"{'-' * 20} WARNING: Function took longer than 0.5s {'-' * 20}")
-        return _result
-
-    def __repr__(self):
-        return f"Function '{self.fn.__name__}' called {self.times_called} times. Total time: {self.accumulated_time:.2f}s."
 
 
 class ast_log:
@@ -74,7 +31,9 @@ class ast_log:
         ...     pass
     """
 
-    def __init__(self, level: int, name: Optional[str] = None, message: Optional[str] = None):
+    def __init__(
+        self, level: int, name: Optional[str] = None, message: Optional[str] = None
+    ):
         self.level = level
         self.logname = name
         self.logmsg = message
@@ -95,7 +54,9 @@ class ast_log:
                     f"Error during execution of {self.logmsg}. See traceback below",
                 )
             else:
-                self.log.log(self.level, f"Function {self.logname} Complete. Result: {result}")
+                self.log.log(
+                    self.level, f"Function {self.logname} Complete. Result: {result}"
+                )
                 return result
 
         return wrapper
