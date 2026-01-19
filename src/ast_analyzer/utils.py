9c313aac41299c88e0a48b6ef2b7531e0a7889d7
Dave Moran <dave.moran118@gmail.com>
2026-01-19 10:03:59 -0500

    ASTANA-8: add final filtering

--- src/ast_analyzer/utils.py
+++ src/ast_analyzer/utils.py
@@ -141,30 +141,7 @@ def filter_by_gitignore(files, ignore_file):
             yield file
 
 
-filtered_files = filter_by_gitignore(files, ".gitignore")
+filtered_files = filter_python_files(filter_by_gitignore(files, ".gitignore"))
 
 for file in filtered_files:
     print(file)
-
-
-def lines_starting_with_vowel(lines):
-    for line in lines:
-        if line[0].lower() in "aeiou":
-            yield line
-
-
-def lines_containing_keyword(lines, keyword):
-    for line in lines:
-        if keyword.lower() in line:
-            yield line
-
-
-def strip_and_uppercase(lines):
-    for line in lines:
-        yield line.strip().upper()
-
-
-def lines_with_numbers_extracted(lines):
-    for line in lines:
-        numbers = re.findall(r"\d+", line)
-        yield numbers
