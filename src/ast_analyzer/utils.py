a20e7f3ec0a454c544bc0980b46f82369e578ad9
Dave Moran <dave.moran118@gmail.com>
2026-01-19 09:23:52 -0500

    ASTANA-8: import function from exercises

--- src/ast_analyzer/utils.py
+++ src/ast_analyzer/utils.py
@@ -74,7 +74,9 @@ class ast_log:
         ...     pass
     """
 
-    def __init__(self, level: int, name: Optional[str] = None, message: Optional[str] = None):
+    def __init__(
+        self, level: int, name: Optional[str] = None, message: Optional[str] = None
+    ):
         self.level = level
         self.logname = name
         self.logmsg = message
@@ -95,7 +97,50 @@ class ast_log:
                     f"Error during execution of {self.logmsg}. See traceback below",
                 )
             else:
-                self.log.log(self.level, f"Function {self.logname} Complete. Result: {result}")
+                self.log.log(
+                    self.level, f"Function {self.logname} Complete. Result: {result}"
+                )
                 return result
 
         return wrapper
+
+
+import re
+
+
+def read_lines(filepath):
+    with open(filepath) as f:
+        for line in f:
+            yield line
+
+
+ft = read_lines("./test.md")
+
+
+def lines_starting_with_vowel(lines):
+    for line in lines:
+        if line[0].lower() in "aeiou":
+            yield line
+
+
+def lines_containing_keyword(lines, keyword):
+    for line in lines:
+        if keyword.lower() in line:
+            yield line
+
+
+def strip_and_uppercase(lines):
+    for line in lines:
+        yield line.strip().upper()
+
+
+def lines_with_numbers_extracted(lines):
+    for line in lines:
+        numbers = re.findall(r"\d+", line)
+        yield numbers
+
+
+result = lines_with_numbers_extracted(ft)
+
+for item in result:
+    print(item)
