48245b438193da9efdce06b280e7cecadbf8eced
Dave Moran <dave.moran118@gmail.com>
2026-01-22 05:14:49 -0500

    ASTANA-6: move AnalysisResult to separate class file

---
+++ src/ast_analyzer/classes/AnalysisResult.py
@@ -0,0 +1,30 @@
+class AnalysisResult:
+    """
+    Takes all of the findings from the CodeAnalyzer class and generates a report
+    to share with our user
+    """
+
+    def __init__(self):
+        self.results = []
+
+    def __repr__(self):
+        return f"AnalysisResult(results={self.results})"
+
+    def __str__(self):
+        return f"Analysis Complete! There are {len(self)} changes to implement"
+
+    def __len__(self):
+        """Return the number of items in the results list"""
+        raise NotImplementedError("ASTANA-6 will implement")
+
+    def __bool__(self):
+        """Return whether or not theres at least 1 result in the result list"""
+        raise NotImplementedError("ASTANA-6 will implement")
+
+    def __getitem__(self, index):
+        """Get a specific item from our results list"""
+        return self.results[index]
+
+    def __add__(self, other):
+        """Combine with results from other scans"""
+        raise NotImplementedError("ASTANA-6 will implement")
