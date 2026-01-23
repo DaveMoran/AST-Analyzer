6cdde1cfbfbae37ec00dfa6e979a3ff73433b455
Dave Moran <dave.moran118@gmail.com>
2026-01-22 23:50:01 -0500

    ASTANA-11: change scopes for results

--- src/ast_analyzer/main.py
+++ src/ast_analyzer/main.py
@@ -12,18 +12,18 @@ from ast_analyzer.ASTNode import ASTNode
 from ast_analyzer.generators.file_traversal import get_working_files
 from ast_analyzer.parser import Parser
 from ast_analyzer.analyzer import CodeAnalyzer
+from ast_analyzer.classes.AnalysisResult import AnalysisResult
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
     # Step 1: Ask user for directory of files
-    directory = (
-        "./src/ast_analyzer/test"  # TODO - swap out with CLI command when we get to that story
-    )
+    directory = "./src/ast_analyzer/test"  # TODO - swap out with CLI command when we get to that story
 
     # Step 2: Filter out all invalid files from directory
     working_files = get_working_files(directory)
+    results = AnalysisResult()
 
     # Step 3: Start iterating through each file
     for file in working_files:
@@ -35,9 +35,13 @@ def main():
                 dedented_code = textwrap.dedent(content)
                 test_tree = ast.parse(dedented_code)
                 node = ASTNode(test_tree)
-                analyzer = CodeAnalyzer(node)
-                results = analyzer.analyze()
-                print(results)
+
+                # Step 6: Run the nodes through our analysis
+                analyzer = CodeAnalyzer(node, results)
+
+                # Step 7: Generate a report based on findings
+                new_result = analyzer.analyze()
+                results += new_result
 
         except FileNotFoundError:
             logging.exception(f"File not found: {file}")
@@ -48,9 +52,8 @@ def main():
         except UnicodeDecodeError:
             logging.exception(f"{file} contains encoding issues")
 
-    # Step 6: Run the nodes through our analysis - ASTANA-6
-    # Step 7: Generate a report based on findings - ASTANA-11
-    print("Hello world")
+    # Step 8: Print results to user
+    print(results)
 
 
 if __name__ == "__main__":
