d30da506d66cc4255a2f3edeb0ea8f0fb95bccb8
Dave Moran <dave.moran118@gmail.com>
2026-01-22 23:31:25 -0500

    ASTANA-11: create new class for function calls

---
+++ src/ast_analyzer/classes/NodeVisitors.py
@@ -0,0 +1,26 @@
+"""
+Group of classes that maintain all of the Node Visitors we'll be using for our
+AST Analyzer
+"""
+
+import ast
+
+
+class FunctionCallCounter(ast.NodeVisitor):
+    def __init__(self):
+        self.count = 0
+
+    def __str__(self):
+        return f"Number of functions: {self.count}"
+
+    def visit_FunctionDef(self, node):
+        """Called when a function call (ast.Call node) is encountered."""
+        self.count += 1
+        # Continue visiting child nodes to find nested calls, arguments, etc.
+        self.generic_visit(node)
+
+    def visit_AsyncFunctionDef(self, node):
+        """Called when a function call (ast.Call node) is encountered."""
+        self.count += 1
+        # Continue visiting child nodes to find nested calls, arguments, etc.
+        self.generic_visit(node)
